507f1fd0080819f69b1cc3c92e11bd6dc5f050f5
MuhammadRamadhanuYusril <107542176+MuhammadRamadhanuYusril@users.noreply.github.com>
2023-06-26 00:50:54 +0700

    Update 2

--- main.js
+++ main.js
@@ -10,3 +10,145 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
   cart.classList.remove("active");
 };
+
+// Making add to cart
+// Cart working JS
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+// Making Function
+function ready() {
+  // Remove Item From Cart
+  var removeCartButtons = document.getElementsByClassName("cart-remove");
+  for (var i = 0; i < removeCartButtons.length; i++) {
+    var button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+    updateTotal();
+  }
+
+  //Quantity Change
+  var quantityInputs = document.getElementsByClassName("cart-quantity");
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+    updateTotal();
+  }
+  // Add to cart
+  var addCart = document.getElementsByClassName("add-cart");
+  for (var i = 0; i < addCart.length; i++) {
+    var button = addCart[i];
+    button.addEventListener("click", addCartClicked);
+    updateTotal();
+  }
+
+  // Remove Cart Item
+  function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+  }
+  // Quantity change
+  function quantityChanged(event) {
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+      input.value = 1;
+      updateTotal();
+    }
+    updateTotal();
+  }
+
+  // Add cart function
+  function addCartClicked(event) {
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title =
+      shopProducts.getElementsByClassName("product-title")[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innerText;
+    var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    addProductToCart(title, price, productImg);
+    updateTotal();
+  }
+
+  function addProductToCart(title, price, productImg) {
+    var cartShopBox = document.createElement("div");
+    cartShopBox.classList.add("cart-box");
+    var cartItems = document.getElementsByClassName("cart-content")[0];
+    var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+    for (var i = 0; i < cartItemsNames.length; i++) {
+      if (cartItemsNames[i].innerText == title) {
+        alert("you have already added this item to cart");
+        return;
+      }
+    }
+    var cartBoxContent = `
+    <div class="cart-box">
+    <img src="${productImg}" alt="" class="cart-img" />
+    <div class="detail-box">
+      <div class="cart-product-title">${title}</div>
+      <div class="cart-price">${price}</div>
+      <input
+        type="number"
+        name=""
+        id=""
+        value="1"
+        class="cart-quantity"
+      />
+    </div>
+    <!--Remove Item-->
+    <i class="bx bx-trash-alt cart-remove"></i>
+  `;
+    cartShopBox.innerHTML = cartBoxContent;
+    cartItems.append(cartShopBox);
+    cartShopBox
+      .getElementsByClassName("cart-remove")[0]
+      .addEventListener("click", removeCartItem);
+    cartShopBox
+      .getElementsByClassName("cart-quantity")[0]
+      .addEventListener("change", quantityChanged);
+    updateTotal();
+  }
+  //Update Total
+  function updateTotal() {
+    var cartContent = document.getElementsByClassName("cart-content")[0];
+    var cartBoxes = cartContent.getElementsByClassName("cart-box");
+    var total = 0;
+
+    for (var i = 0; i < cartBoxes.length; i++) {
+      var cartBox = cartBoxes[i];
+      var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+      var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+      var price = parseFloat(priceElement.innerText.replace("Rp.", ""));
+      // var quantity = parseInt(quantityElement.value, 10); // coba coba
+      var quantity = quantityElement.value;
+      total += price * quantity;
+    }
+    document.getElementsByClassName("total-price")[0].innerText = "Rp." + total;
+  }
+  //taro disini
+  updateTotal();
+}
+
+// var quantityInputs = document.getElementsByClassName("cart-quantity");
+// for (var i = 0; i < quantityInputs.length; i++) {
+//   quantityInputs[i].addEventListener("input", updateTotal);
+// }
+// updateTotal();
+
+// ini yg error , taro dibawah update total, updatetotal() nya ditauh di quantity change diantara kurung kurawal bawah
+// function updatetotal() {
+//   var cartContent = document.getElementsByClassName("cart-content")[0];
+//   var cartBoxes = cartContent.getElementsByClassName("cart-box")[0];
+//   var total = 0;
+//   for (var i = 0; i < cartBoxes.length; i++) {
+//     var cartBox = cartBoxes[i];
+//     var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+//     var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+//     var price = parseFloat(priceElement.innerText.replace("$", ""));
+//     var quantity = quantityElement.value;
+//     total += price * quantity;
+
+//     document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+//   }
+// }
